2969002e733b200e5ee0cda9991748d5d43bc3c2
Dim <dmotorny@gmail.com>
2016-12-29 07:00:39 +0200

    new

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('hello my friendssx');
+const btn = document.querySelector('.btn');
+
+function pow(x, n) {
+    if (n != 1) {
+        return x * pow(x, n - 1);
+    } else {
+        return x;
+    }
+}
+
+btn.addEventListener('click', e => {
+    const x = document.querySelector('.first_num').value;
+    const n = document.querySelector('.second_num').value;
+    const result = pow(x, n);
+    alert(result);
+});
+
